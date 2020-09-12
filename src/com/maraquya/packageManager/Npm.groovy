7d40d86a74698a76f0aa8d9b0446036a0d1a08b5
Jamie Ynonan <jamiea31@gmail.com>
2020-09-12 12:47:59 -0500

    refactor node

--- src/com/maraquya/packageManager/Npm.groovy
+++ src/com/maraquya/packageManager/Npm.groovy
@@ -3,18 +3,15 @@ package com.maraquya.packageManager
 import com.maraquya.virtualization.Docker
 
 class Npm implements PackageManager {
-    private String projectPath = ''
     private String dockerImage = "jamieynonan/nodechrome:0.1.0-alpine"
     private Docker docker
+    private String absoluteProjectPath
     private script
 
-    Npm(script, Docker docker) {
+    Npm(script, Docker docker, String absoluteProjectPath) {
         this.script = script
         this.docker = docker
-    }
-
-    void setProjectPath(String projectPath) {
-        this.projectPath = projectPath
+        this.absoluteProjectPath = absoluteProjectPath
     }
 
     void setDockerImage(String dockerImage) {
@@ -22,14 +19,14 @@ class Npm implements PackageManager {
     }
 
     void install() {
-        this.execCommand("npm install")
+        this.execCommand("npm install --no-optional --no-package-lock --only=dev")
     }
 
     private void execCommand(final String command) {
         this.docker.run(
             this.dockerImage,
             command,
-            "-v ${this.script.env.WORKSPACE}/${this.projectPath}:/app -w /app"
+            "-v ${this.absoluteProjectPath}:/app -w /app"
         )
     }
 
