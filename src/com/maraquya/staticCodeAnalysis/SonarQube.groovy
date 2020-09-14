adf83d8ca52d325909dbae3fc4180ef77e7a9cf7
Jamie Ynonan <jamiea31@gmail.com>
2020-09-14 01:21:36 -0500

    upgrade sonar cli version

--- src/com/maraquya/staticCodeAnalysis/SonarQube.groovy
+++ src/com/maraquya/staticCodeAnalysis/SonarQube.groovy
@@ -3,7 +3,7 @@ package com.maraquya.staticCodeAnalysis
 import com.maraquya.virtualization.Docker
 
 class SonarQube implements Serializable, StaticCodeAnalysis {
-    private String dockerImage = "sonarsource/sonar-scanner-cli:4.3"
+    private String dockerImage = "sonarsource/sonar-scanner-cli:4.4"
     private Docker docker
     private String absoluteProjectPath
     private script
@@ -14,20 +14,15 @@ class SonarQube implements Serializable, StaticCodeAnalysis {
         this.absoluteProjectPath = absoluteProjectPath
     }
 
-    void setDockerImage(String dockerImage) {
-        this.dockerImage = dockerImage
-    }
-
     void run() {
         this.script.steps.withCredentials([
             this.script.string(credentialsId: 'sonar', variable: 'SONAR_TOKEN')
         ]) {
             this.docker.run(
                 dockerImage,
-                "sonar-scanner -Dsonar.login=${this.script.env.SONAR_TOKEN} ",
+                "sonar-scanner -Dsonar.login=${this.script.env.SONAR_TOKEN}",
                 "-e SRC_PATH=\"/app\" -e SONAR_HOST_URL=${this.script.env.SONAR_HOST_URL} " +
-                    "-v ${this.absoluteProjectPath}:/app " +
-                    "-w /app"
+                    "-v ${this.absoluteProjectPath}:/app -w /app"
             );
         }
     }
